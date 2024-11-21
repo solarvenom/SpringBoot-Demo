b7799f1681d8d262463f662fc928fc60aabccb87
solarvenom <contact@solarvenom.com>
2024-11-21 15:11:08 +0100

    remove .DS_Store, rename project

--- src/test/java/demo/ecommerce/BackendApiApplicationTests.java
+++ src/test/java/demo/ecommerce/BackendApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package demo.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApiApplicationTests {
 
 	@Test
 	void contextLoads() {
